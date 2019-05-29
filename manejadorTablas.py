a44cfb9f3eb13bd9c36f6acd89034c5928b6c434
IvanPinna <ivanpinaarevalo@gmail.com>
2019-05-29 21:26:03 +0200

    Añadido fichero de entrada

--- manejadorTablas.py
+++ manejadorTablas.py
@@ -1,10 +1,13 @@
+import sys
+
 tablaFunciones = {}
 
 class manejadorFunciones():
   selectLista = 0
 
   def selector(self, id):
-    self.selectLista = id
+    if id in tablaFunciones:
+      self.selectLista = id
 
   def crearTabla(self, id, list):
     global tablaFunciones
@@ -18,8 +21,15 @@ if __name__ == '__main__':
   p = manejadorFunciones()
   lista = ['1','2']
   lista2 = ['3', '4']
-  p.crearTabla('main', lista)
-  p.crearTabla('funcion', lista2)
+  #p.crearTabla('main', lista)
+  #p.crearTabla('funcion', lista2)
+  #p.devolverTabla()
 
-  p.selector('main')
-  p.devolverTabla()
+  if len(sys.argv) == 2:
+    print("La cadena introducida tiene", sys.argv[1], "caracteres");
+  else:
+    print("Este programa necesita un parámetro");
+
+  dir = './' + sys.argv[1]
+  reader = open(dir, 'r')
+  print(reader.read())
